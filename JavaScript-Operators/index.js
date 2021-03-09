cd0cfafb50a28f0ece20ae534de4fa533f0529d9
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-03-09 22:19:28 +0530

    JavaScripit Operators

---
+++ JavaScript-Operators/index.js
@@ -0,0 +1,33 @@
+
+let x = 3;
+let y = 5;
+
+//arthimatic opr
+// console.log(x + y);
+// console.log(x - y);
+// console.log(x * y);
+// console.log(x / y);
+// console.log(x % y);
+// console.log(x ** y);
+
+//Increment/decrement
+// console.log(x++);
+// console.log(++x);
+// console.log(y++);
+// console.log(++y);
+
+//assignment opr
+//x += 5;
+
+//comparision opr 
+// console.log(x > 0);
+// console.log(x < y);
+// console.log(x >= 0);
+// console.log(x <= 0);
+
+//Equility opr
+// console.log(x === y); //strict equility opr [compare both type and value]
+// console.log(x == y); //Lose equility opr [compare only value]
+// console.log(x !== y);
+
+
