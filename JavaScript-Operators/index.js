790a767e7c48a693c03e15e83720ec27e7b6d13e
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2021-03-10 10:52:49 +0530

    JavaScript Operators

--- JavaScript-Operators/index.js
+++ JavaScript-Operators/index.js
@@ -30,4 +30,56 @@ let y = 5;
 // console.log(x == y); //Lose equility opr [compare only value]
 // console.log(x !== y);
 
+// Terrinary opr
+// let points = 100;
+// let type = points > 100 ? 'gold' : 'silver';
+// console.log(type);
+
+// LOgical Opr with bollean
+// console.log(true && false); //AND
+// console.log(true || false); //OR
+//console.log(true =! false);  //NOT
+
+// LOgical Opr with-out bollean
+   // Falsy:
+       // undefined
+       // null
+       // 0
+       // ''
+       // NaN
+
+   // Truthy (other than Falsy)
+
+// let userColor = 'red'; //( replaace red -> undefined)
+// let defaultColor = 'blue';
+// let curentColor = userColor || defaultColor;
+
+// console.log(curentColor);
+
+
+// BitWise opr (bit by bit convervsion)
+// console.log(1 | 2); // bitwise or
+// console.log(1 & 2); // bitwise and 
+
+
+// Precedence opr
+// console.log(2+3*4);
+// console.log((2+3)*4);
+
+// Question
+
+let a = 'red';
+let b = 'blue';
+console.log(a);
+console.log(b);
+let c = b;
+b= a;
+a = c;
+console.log(a);
+console.log(b);
+
+
+
+
+
 
